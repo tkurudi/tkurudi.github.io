94a3d1d6d45d98ab8b8b2754a479a7131adcbb4f
tkurudi <t.kurudi209@gmail.com>
2018-10-01 22:54:46 +1300

    ms

---
+++ p5/framework/ms.js
@@ -0,0 +1,97 @@
+//minesweeper
+
+function make2DArray(cols, row){
+var arr = new Array(cols);
+for (var i = 0; i < arr.length; i++){
+	arr[i] = new Array(rows);
+}
+return arr;
+
+}
+
+
+
+
+var grid;
+var cols;
+var rows;
+var w = 20;
+
+var totalMines = 30;
+
+
+function setup(){
+	createCanvas(201, 201);
+	cols = floor(width / w);
+	rows = floor(height / w);
+	grid = make2DArray(cols, rows);
+	for (var i = 0; i < cols; i++) {
+	for (var j = 0; j < rows; j++) {
+	grid[i][j] = new Cell(i, j, w);
+	}
+}
+
+//pick totalmines spots
+var options = [];
+for (var i = 0; i < cols; i++) {
+	for (var j = 0; j < rows; j++) {
+		options.push([i, j]);
+
+		}
+	}
+
+for (var n = 0; n < totalMines; n++){
+	var index = floor(random(options.length));
+	var choice = options[index];
+	var i = choice[0];
+	var j = choice[1];
+	//delete the spot so its no longer an option
+	options.splice(index, 1);
+	grid[i][j].mine = true;
+}
+
+for (var i = 0; i < cols; i++) {
+	for (var j = 0; j < rows; j++) {
+	grid[i][j].countMines();
+		}
+	}
+}
+
+function gameOver(){
+	for (var i = 0; i < cols; i++) {
+	for (var j = 0; j < rows; j++) {
+	grid[i][j].revealed = true;
+		}
+	}
+}
+
+
+
+
+
+function  mousePressed() {
+
+for (var i = 0; i < cols; i++) {
+	for (var j = 0; j < rows; j++) {
+	if (grid[i][j].contains(mouseX, mouseY)) {
+	grid[i][j].reveal();
+
+	if (grid[i][j].mine){
+		gameOver();
+
+			
+		}
+	 }
+	}
+  }
+} 
+
+
+function draw() {
+background(255);
+for (var i = 0; i < cols; i++) {
+	for (var j = 0; j < rows; j++) {
+	grid[i][j].show();
+	}
+  }
+} 
